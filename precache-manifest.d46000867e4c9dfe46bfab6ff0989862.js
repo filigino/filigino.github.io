c6bd7ba20b4b24b94e1498302e152aaf6a31120b
Gino Salayo <gino.salayo@gmail.com>
2019-12-17 12:02:42 -0500

    React launch

---
+++ precache-manifest.d46000867e4c9dfe46bfab6ff0989862.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16334c74bf36615f23baad3af642ea3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "616099aac57e378caea3",
+    "url": "/static/css/2.03fe6687.chunk.css"
+  },
+  {
+    "revision": "6b6d4565be631c7e40ec",
+    "url": "/static/css/main.6399724e.chunk.css"
+  },
+  {
+    "revision": "616099aac57e378caea3",
+    "url": "/static/js/2.ba7a6b11.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ba7a6b11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b6d4565be631c7e40ec",
+    "url": "/static/js/main.850ccd1d.chunk.js"
+  },
+  {
+    "revision": "5814774fb3a75375ee23",
+    "url": "/static/js/runtime-main.1e8898d2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
